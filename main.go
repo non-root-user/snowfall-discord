188f94f333da8d0b7383473186003e5ef1c90e16
BenekDoesHorses <b@horsi.fi>
2021-02-19 21:54:46 +0100

    ⚡Basic RabbitMQ integration

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strings"
 	"sync"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/streadway/amqp"
 )
 
 func main() {
@@ -15,12 +17,49 @@ func main() {
 	if token == "" {
 		panic("Missing token environment variable")
 	}
+	//URL formated such as "amqp://guest:guest@server.localhost:5672/"
+	rabbit := os.Getenv("RABBIT_URL")
+	if token == "" {
+		panic("Missing rabbit url environment variable")
+	}
 
 	dg, err := discordgo.New("Bot " + token)
-	if err != nil {
-		fmt.Println("error creating Discord session,", err)
-		return
-	}
+	failOnError(err, "error creating Discord session")
+
+	conn, err := amqp.Dial(rabbit)
+	failOnError(err, "error connecting to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"SendDiscordValidationMessage", // name
+		false,                          // durable
+		false,                          // delete when unused
+		false,                          // exclusive
+		false,                          // no-wait
+		nil,                            // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
 
 	dg.AddHandler(sendMessage)
 	dg.AddHandler(commandHandle)
@@ -67,4 +106,10 @@ func commandHandle(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	fmt.Println(args)
 
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
 }
